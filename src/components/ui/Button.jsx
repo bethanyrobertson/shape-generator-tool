e78a3287f79d07df8b3a4af05e4dd779357b1a50
bethanyrobertson <bethany@bethanyrobertson.com>
2025-03-11 17:57:22 -0400

    Update Button.jsx

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -3,7 +3,7 @@ import { cn } from "../../lib/utils"
 
 const Button = forwardRef(({ className, variant = "default", size = "default", children, ...props }, ref) => {
   const baseStyles =
-    "inline-flex items-center justify-center rounded-md font-medium transition-colors focus-visible:outline-white focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background"
+    "inline-flex items-center justify-center text-white rounded-md font-medium transition-colors focus-visible:outline-white focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background"
 
   const variants = {
     default: "bg-primary text-white hover:bg-primary/90",
